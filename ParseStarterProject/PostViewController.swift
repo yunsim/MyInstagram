efd742a4fc52700b0504f12c4800f3917cd599aa
Yunsi Mou <yunsim@uw.edu>
2017-08-22 13:57:59 -0700

    add post image function

---
+++ ParseStarterProject/PostViewController.swift
@@ -0,0 +1,96 @@
+//
+//  PostViewController.swift
+//  ParseStarterProject-Swift
+//
+//  Created by Yunsi Mou on 8/22/17.
+//  Copyright © 2017 Parse. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class PostViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+
+    var activityIndicator = UIActivityIndicatorView()
+    
+    @IBOutlet var imageToPost: UIImageView!
+    @IBAction func chooseImage(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        imagePicker.allowsEditing = false
+        
+        self.present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageToPost.image = image
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBOutlet var messageText: UITextField!
+    
+    @IBAction func postImage(_ sender: Any) {
+        activityIndicator = UIActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+        activityIndicator.center = self.view.center
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.gray
+        view.addSubview(activityIndicator)
+        activityIndicator.startAnimating()
+        UIApplication.shared.beginIgnoringInteractionEvents()
+        
+        let post = PFObject(className: "Posts")
+        post["message"] = messageText.text
+        post["userid"] = PFUser.current()?.objectId!
+        
+        let imageData = UIImagePNGRepresentation(imageToPost.image!)
+        let imageFile = PFFile(name: "image.png", data: imageData!)
+        post["imageFile"] = imageFile
+        
+        post.saveInBackground { (success, error) in
+            self.activityIndicator.stopAnimating()
+            UIApplication.shared.endIgnoringInteractionEvents()
+            
+            if error != nil {
+                self.createAlert(title: "Could not post image", message: "Please try again")
+            } else {
+                self.createAlert(title: "Image posted", message: "You image has been posted successfully")
+                self.messageText.text = ""
+                self.imageToPost.image = UIImage(named: "person-1824147_960_720.png")
+            }
+        }
+    }
+    
+    func createAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (action) in
+            alert.dismiss(animated: true, completion: nil)
+        }))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
